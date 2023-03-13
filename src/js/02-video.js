97a9b3edc4e202f19b99cb50207380c977b7c80d
Marina Khmarskaya <myhappyday@ukr.net>
2023-03-13 07:06:39 +0200

    Add Task 2 Module-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import VimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const playerIframe = document.querySelector("#vimeo-player");
+const player = new VimeoPlayer(playerIframe);
+
+player.on(
+  "timeupdate",
+  throttle(data => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem("videoplayer-current-time");
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
