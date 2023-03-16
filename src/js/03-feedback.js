52120c42f75dfc2aae2216d576dd158289a0ef0e
Marina Khmarskaya <myhappyday@ukr.net>
2023-03-16 02:39:24 +0200

    Add Task 3 Module-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector(".feedback-form input");
+const messageInput = form.querySelector(".feedback-form textarea");
+
+form.addEventListener("submit", onFormSubmit);
+form.addEventListener("input", throttle(onFormInput, 500));
+
+populateFormData();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (emailInput.value && messageInput.value) {
+    const formData = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    console.log(formData);
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
+
+function onFormInput(event) {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateFormData() {
+  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedFormData) {
+    emailInput.value = savedFormData.email;
+    messageInput.value = savedFormData.message;
+  }
+}
